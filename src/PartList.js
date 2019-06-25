ed60883a4402ddb48d0465f14f7ba5e74ad0443b
Paul Han <kyutae.han93@gmail.com>
2019-06-25 16:40:30 -0400

    minor bug change on app.css

--- src/PartList.js
+++ src/PartList.js
@@ -4,7 +4,7 @@ function PartList(props){
   let chatList=props.participants.map((participant)=>
     (
     <div className='participant'> 
-      <img src={participant.avatar} alt='avatar' className='avatar'/>
+      <img src={participant.avatar} alt='avatar' className='avatar-part'/>
       <h5> {participant.name}</h5>
       <p>
         {participant.onStage?'on stage':'leave stage'}
