a6057b0cb5855640a41df64eb6b0afa7c87112d9
Paul Han <kyutae.han93@gmail.com>
2019-06-25 15:01:49 -0400

    fixed css for chatmessage and also formatting in the .js

--- src/ChatMessage.js
+++ src/ChatMessage.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import './ChatMessage.css'
+
+function getTime(date){
+  let hours= date.getHours();
+  let AMPM= hours>=12?'PM':'AM';
+  if(hours>12){
+    hours-=12;
+  }
+
+  return `${hours}:${date.getMinutes()} ${AMPM}`;
+}
 
 function ChatMessage(props){
   if(props.message){
@@ -9,21 +20,44 @@ function ChatMessage(props){
           <h6 className="message-body-header">
             {props.name}
           </h6>
-          <span className="message-body-time"> {(new Date(props.time)).toTimeString()} </span>
-        </div>
-        <div className="message-body-content">
-          <p>{props.message}</p>
+          <span className="message-body-time"> {getTime(new Date(props.time))} </span>
+          <div className="message-body-content">
+            <p>{props.message}</p>
+          </div>
         </div>
       </div>
     );
   }
   else{
+    let message;
     if(props.type==='join'){
-
+      message='joined';
+    }
+    else if(props.type==='thumbs-up'){
+      message='gave a thumbs up'
+    }
+    else if(props.type==='thumbs-down'){
+      message='gave a thumbs down'
+    }
+    else if(props.type==='raise-hands'){
+      message='raised their hand'
+    }
+    else if(props.type==='clap'){
+      message='clapped'
+    }
+    else if(props.type==='leave'){
+      message='left'
+    }
+    else if(props.type==='join-stage'){
+      message='joined the stage'
+    }
+    else if(props.type==='leave-stage'){
+      message='left the stage'
     }
     return(
-      <div classNAme="system-message">
-        <strong>{props.name}</strong>
+      <div className="system-message">
+        <strong>{props.name}</strong> 
+        <span> {message}</span>
       </div>
 
 
