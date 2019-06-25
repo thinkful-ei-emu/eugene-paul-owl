c30303a2efcd527cd98b7afeaae2ac9ce53c4cb4
Paul Han <kyutae.han93@gmail.com>
2019-06-25 15:44:39 -0400

    fixed css

--- src/Stage.js
+++ src/Stage.js
@@ -1,26 +1,21 @@
 import React from 'react';
 
 function gatherOnStage(list){
-  return  list.filter((part)=>part.OnStage);
+  return  list.filter((part)=>part.onStage);
 }
 
 function Stage(props) {
   let stageParts=gatherOnStage(props.participants);
   let jsxList=stageParts.map((part)=>(
     <div className='part-on-stage'>
-      <div className="name">{}</div>
-      <div className="avatar"></div>
+      <div className="name">{part.name}</div>
+      <div className="avatar">picture here</div>
     </div>
     )
   );
   if(stageParts.length) {
     return (
       <div>
-        <div><h3>{props.name}</h3></div>
-        <div className = "avatar-container">
-          <img src = {props.avatar} alt ="avatar"/>
-        </div>
-
         {jsxList}
       </div>
     )
